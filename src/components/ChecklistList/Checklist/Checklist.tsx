d0dfb4b249969dadeb8c3a960e13e3a9502d22af
PiyushDaulatkar <piyushdaulatkar03@gmail.com>
2025-09-07 18:37:51 +0530

    Add edit, startEdit, cancelEdit, confirmEdit functionality to CheckListItem.

--- src/components/ChecklistList/Checklist/Checklist.tsx
+++ src/components/ChecklistList/Checklist/Checklist.tsx
@@ -1,14 +1,31 @@
 import ChecklistItem from "./ChecklistItem/ChecklistItem";
 
-import ChecklistList from "../../../../mockdata/checkListItems.json";
+import ChecklistListData from "../../../../mockdata/checkListItems.json";
+import { useState } from "react";
 
 function Checklist() {
-  function handleEditItem(id: number) {
-    console.log("Edit item with id:", id);
+  const [ChecklistList, setChecklistList] =
+    useState<{ id: string; text: string }[]>(ChecklistListData);
+  const [currentEditingId, setCurrentEditingId] = useState<string | null>(null);
+
+  function handleDeleteItem(id: string | null) {
+    console.log("Delete item with id:", id);
+    const filteredList = ChecklistList.filter((item) => item.id !== id);
+    setChecklistList(filteredList);
   }
 
-  function handleDeleteItem(id: number) {
-    console.log("Delete item with id:", id);
+  function handleEditItem(id: string | null) {
+    console.log("Edit item with id:", id);
+    setCurrentEditingId(id);
+  }
+
+  function handleYesEdit(id: string, editedText: string) {
+    console.log("Yes edit item with id:", id, "New text:", editedText);
+    const updatedList = ChecklistList.map((item) =>
+      item.id === id ? { ...item, text: editedText } : item
+    );
+    setChecklistList(updatedList);
+    setCurrentEditingId(null);
   }
 
   return (
@@ -17,9 +34,13 @@ function Checklist() {
         return (
           <ChecklistItem
             key={item.id}
+            id={item.id}
             checklistItemText={item.text}
-            onEditItem={() => handleEditItem(item.id)}
-            onDeleteItem={() => handleDeleteItem(item.id)}
+            currentEditingId={currentEditingId}
+            onStartEdit={handleEditItem}
+            onDeleteItem={handleDeleteItem}
+            onConfirmEdit={handleYesEdit}
+            onCancelEdit={() => setCurrentEditingId(null)}
           />
         );
       })}
